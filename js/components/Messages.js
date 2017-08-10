686013267d8c30dbb6856451c6ff961c8bc9131b
palexis3 <alexispatrick77@gmail.com>
2017-08-10 03:18:27 -0700

    Implemented Messages components with passed in messages prop

---
+++ js/components/Messages.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Message from './Message';
+
+class Messages extends React.Component {
+
+  // React 'componentDidUpdate' lifecycle function is used to scroll to bottom of list
+  componentDidUpdate() {
+    const divObj = document.getElementById('messagesList');
+    divObj.scrollTop = divObj.scrollHeight;
+  }
+
+  render() {
+    // loop through all mesages passed through prop to get the list of messages in chat
+    const messages_all = this.props.messages.map((message, i) => {
+       return (
+         <Message
+           key={i}
+           username={message.username}
+           message={message.message}
+           wasitMe={message.wasItMe} />
+       );
+    )};
+
+    return (
+      <div className="messages" id="messagesList">
+        { messages_all }
+      </div>
+    );
+  }
+}
+
+Messages.defaultProps = {
+  messages: ["Hello world"]
+};
+
+export default Messages;
